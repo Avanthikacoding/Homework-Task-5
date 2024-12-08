0922c923438513055954d938ead1cb8246e13aef
Avanthikacoding <aviedayilliam@gmail.com>
2024-12-08 13:42:01 +0000

    Add files via upload

---
+++ Homework Task 5.py
@@ -0,0 +1,5 @@
+summer_clothes = input("Enter what clothes you are wearing:")
+if summer_clothes == "a shirt":
+    print("You are wearing the right clothes! YOur good to go! Bye! Have fun in the park 😜")
+else: 
+    print("You have the wrong clothes for this weather! Go change right now 😡 and only come back when you are weraing the right clothes for summer🙅‍♀️! I ddin't expect a mature enough child to be wearing that for summer 🤦‍♀️")
